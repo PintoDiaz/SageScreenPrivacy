d2809b89b7a80cc8e8017a0b702d2ddbd3ca0cc6
Roger Pintó <roger.pinto@sage.com>
2020-10-02 09:16:45 +0200

    + Setup

---
+++ SageScreenPrivacy/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SageScreenPrivacy
+//
+//  Created by Pinto Diaz, Roger on 10/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
